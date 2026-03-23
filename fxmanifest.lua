0b0dd4e95e135162bb653d6d24bbc89be8f9656c
FoxcodeLeet42 <essalhiamine212@gmail.com>
2026-03-23 22:20:33 +0100

    updates

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+loadscreen 'dist/index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'dist/index.html',
+    'dist/assets/*',
+    'dist/*.jpg',
+    'dist/*.svg',
+    'dist/*.mp3',
+    'dist/*.mp4',
+    'dist/*.ttf'
+}
